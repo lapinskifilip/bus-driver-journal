25df39975f8188e6a0ad9f48f8eb4328094f7d90
Filip Łapiński <lapinski.filipp@gmail.com>
2022-11-06 15:01:45 +0100

    Add login and make some styling

--- bdj/bdj/urls.py
+++ bdj/bdj/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include('django.contrib.auth.urls')),
     path('', include("main.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
